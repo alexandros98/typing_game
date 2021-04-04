c044838153c9ffe87f1c998265a5760781fe300b
alexandros98 <alexproko123@gmail.com>
2021-04-04 18:00:00 +0300

    Some Gameplay and visual changes.
    Each level now spawns a specific amount of words(starting from 3,increased by 1 each level.
    New class Anouncer lets me print things at the center of the screen(for example it is used to anounce level changes).
    Changed all the colors to prevent eye damage.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys,pygame
 from pygame.constants import KSCAN_BACKSPACE, K_ESCAPE, MOUSEBUTTONDOWN, TEXTINPUT
-from wordGenerator import WordGenerator,Word
+from wordGenerator import WordGenerator,Word,Anouncer
 from random import randint
 
 #inits
@@ -9,28 +9,40 @@ clk =  pygame.time.Clock()
 screenWidth = 1000
 screenHeight = 800
 screen = pygame.display.set_mode((screenWidth,screenHeight))
+pygame.display.set_caption("Typing Game")
+windowIcon = pygame.image.load("window_icon.jpg")
+pygame.display.set_icon(windowIcon)
 mainfont = pygame.font.SysFont(None,40)
 wg =  WordGenerator()
 
 #Target Text Related
 texts = []
-wordSpeed = 20
+wordSpeed = 15
 wordsonscreen = len(texts) #Amount of words displayed on screen.
-wordsTotal = 1 #Total number of words allowed to be displayed on screen.
+wordsTotal = 3 #Total number of words allowed to be displayed on screen.
+spawnFlag = True
+
 
 #User input related
-userinputColor = (50,0,100)
+userinputColor = (255, 255, 84)
 keyPressed = ''
-userinputText = ">"
+userinputStaticColor = (0, 120, 215)
+userinputText = ""
 userInputFont = pygame.font.SysFont(None,50)
+userinputStatic = userInputFont.render(">", True, userinputStaticColor)
 userinput = userInputFont.render(userinputText, True, userinputColor)
-userinputPos = screenWidth/2-(screenWidth/10),screenHeight-(screenHeight/9)
+userinputPos = (screenWidth/2-(screenWidth/10)+userinputStatic.get_size()[0],screenHeight-(screenHeight/9))
+userinputStaticPos = (screenWidth/2-(screenWidth/10),screenHeight-(screenHeight/9))
 counter = 0
 inputHidden = ""
 focusWord = None
-focusFlag = False #Probably a useless variable...
 resetFlag = False
 swapFlag = False
+userInput_ghost = ""
+usrincolor_ghost = (179,179,179)
+usrinfont_ghost = pygame.font.SysFont(None,50)
+usrintxt_ghost = usrinfont_ghost.render("" + userInput_ghost, True, usrincolor_ghost)
+usrintxt_ghost.set_alpha(127)
 
 #Score realted
 botGap = 30 #Gap between the elements on the bottom infos.
@@ -39,21 +51,31 @@ timeTemp = 0
 mins = 0
 secs = 0
 bottomInfosHeight = 40
-bottomTextColor = (0,0,0)
+bottomTextColor = (179,179,179)
 bottomFont = pygame.font.SysFont(None,30)
 correctWords = bottomFont.render("Score: " + str(points), True, bottomTextColor)
 time = bottomFont.render("Time: " + str(timeTemp) + ":" +str(int(timeTemp)), True, bottomTextColor)
 level = 1
-levelAdv = 5 #Every 5 correct words,+1 word on screen.
+levelAdv = 0
 levelText = bottomFont.render("Level: " + str(int(level)), True, bottomTextColor)
 wpm = 0
 wpmTemp = 0
 wpmFormat = "{0:.2f}"
 wordsperMin = bottomFont.render("WPM: " + str(wpm), True, bottomTextColor)
+wordsMissed = 0
+wordsMissedTxt = bottomFont.render("Missed: " + str(wordsMissed), True, bottomTextColor)
 
+#Anouncer related
+anouncerFont = pygame.font.SysFont(None,100)
+anouncer = Anouncer(anouncerFont,screenWidth,screenHeight,bottomInfosHeight)
+alpha = 0
 
 
 while True:
+    #Display the text for the first level anouncer(uses the initialprint variable to check,should oly happen once)
+    if anouncer.initialPrint:
+        anouncer.changeText("Level: " + str(level),2)
+        anouncer.initialPrint = False
     frameTime = clk.tick(60) / 1000
     wordsonscreen = len(texts)
     wpmTemp += frameTime
@@ -65,13 +87,11 @@ while True:
             time = bottomFont.render("Time: " + str(mins) + ":" +str(secs), True, bottomTextColor)
         else:
             time = bottomFont.render("Time: 0" + str(mins) + ":" +str(secs), True, bottomTextColor)
-
         secs+=1
     if secs == 60:
         mins += 1
         secs = 0
         timeTemp = 0
-    print(timeTemp)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
@@ -96,30 +116,35 @@ while True:
         if focusWord != None and resetFlag and focusWord.getWord() == x.getWord():
             focusWord = None
             inputHidden = ''
-            userinputColor = (50,0,100)
-            userinputText = ">"
+            userinputText = ""
             userinput = userInputFont.render(userinputText, True, userinputColor)
             counter = 0
-            print("changed focus")
             x.focusColor(mainfont,False)
+            userInput_ghost = ""
+            usrintxt_ghost = usrinfont_ghost.render("" + userInput_ghost, True, usrincolor_ghost)
+            usrintxt_ghost.set_alpha(127)
             resetFlag = False
         if swapFlag and focusWord != None: #Target word focus change.
             indexTemp = texts.index(focusWord) + 1
             if indexTemp >= len(texts):
                 indexTemp = 0
             counter = 0
-            inputHidden = ''
-            userinputText = ">"
+            inputHidden = ""
+            userinputText = ""
             userinput = userInputFont.render(userinputText, True, userinputColor)
             focusWord = texts[indexTemp]
             focusWord.focusColor(mainfont,True)
+            userInput_ghost = focusWord.getWord()
+            usrintxt_ghost = usrinfont_ghost.render("" + userInput_ghost, True, usrincolor_ghost)
+            usrintxt_ghost.set_alpha(127)
             swapFlag = False
         if counter < len(x.getWord()) and keyPressed == x.getWord()[counter] and x.getPosY() >= 0:
             if focusWord == None or focusWord.getWord() == x.getWord():
                 x.focusColor(mainfont,True)
                 focusWord = x
-                focusFlag = True
-                userinputColor = (50,0,100)
+                userInput_ghost = x.getWord()
+                usrintxt_ghost = usrinfont_ghost.render("" + userInput_ghost, True, usrincolor_ghost)
+                usrintxt_ghost.set_alpha(127)
                 userinputText += keyPressed
                 userinput = userInputFont.render(userinputText, True, userinputColor)
                 counter += 1
@@ -128,55 +153,77 @@ while True:
         
         if x.getPosY() >= screenHeight - bottomInfosHeight:
             texts.remove(x)
-            print("word missed")
+            wordsMissed += 1
+            wordsMissedTxt = bottomFont.render("Missed: " + str(wordsMissed), True, bottomTextColor)
+            userInput_ghost = ""
+            usrintxt_ghost = usrinfont_ghost.render("" + userInput_ghost, True, usrincolor_ghost)
             if x == focusWord:
                 focusWord = None
-                userinputText = ">"
+                userinputText = ""
                 userinput = userInputFont.render(userinputText, True, userinputColor)
                 counter = 0
                 inputHidden = ""
+            #spawnFlag = True
+            levelAdv -= 1
+            
             
     if focusWord != None and inputHidden == focusWord.getWord():
-        wordSpeed += 2
-        focusFlag = False
-        #print(focusWord.getWord())
         inputHidden = ''
-        userinputColor = (0,255,0)
-        userinput = userInputFont.render(userinputText, True, userinputColor)
-        userinputText = ">"
+        userinputText = ""
         counter = 0
         texts.remove(focusWord)
-        #print("Word Completed")
         focusWord = None
         points += 1
         correctWords = bottomFont.render("Score: " + str(points), True, bottomTextColor)
-        
 
-    if wordsonscreen < wordsTotal:
+    #-----------------------------
+    if wordsonscreen < wordsTotal and spawnFlag:
         wordTemp = Word(wg.generateWord(),randint(0,screenWidth),mainfont,wordSpeed)
         if (screenWidth - wordTemp.getPosX()) < wordTemp.getWordWidth():
             wordTemp.adjustWord(screenWidth - wordTemp.getWordWidth())
         texts.append(wordTemp)
+        levelAdv += 1
+    else:
+        spawnFlag = False
 
     if points == levelAdv:
+        wordSpeed += 1
         wordsTotal += 1
-        levelAdv += 5
         level += 1
         levelText = bottomFont.render("Level: " + str(int(level)), True, bottomTextColor)
+        anouncer.changeText("Level: " + str(level),2)
+        spawnFlag = True
+
+
     wpm = points/wpmTemp
     wordsperMin = bottomFont.render("WPM: " + str(wpmFormat.format(wpm)), True, bottomTextColor)
+
     #blits etc..
-    screen.fill((0,0,0))
-    screen.blit(userinput,(userinputPos))
+    screen.fill((18,18,18))
+    screen.blit(usrintxt_ghost,userinputPos)
+    screen.blit(userinput,userinputPos)
+    screen.blit(userinputStatic,userinputStaticPos)
     for x in texts:
         if x.getPosY() >= screenHeight:
             texts.remove(x)
         speedTemp = (x.wordSpeed() + wordSpeed)*frameTime
         x.addWordY(speedTemp)
         screen.blit(x.getWordObj(),(x.getPosX(),x.getPosY()))
-    pygame.draw.rect(screen,(255,0,0),pygame.Rect(0,screenHeight-bottomInfosHeight, screenWidth, bottomInfosHeight))
+    pygame.draw.rect(screen,(64,64,64),pygame.Rect(0,screenHeight-bottomInfosHeight, screenWidth, bottomInfosHeight))
     screen.blit(correctWords,(botGap,screenHeight-(bottomInfosHeight-correctWords.get_size()[1]/2)))
     screen.blit(time,(correctWords.get_size()[0] + botGap*2,screenHeight-(bottomInfosHeight-time.get_size()[1]/2)))
     screen.blit(levelText,(correctWords.get_size()[0] + time.get_size()[0] + botGap*3,screenHeight-(bottomInfosHeight-time.get_size()[1]/2)))
     screen.blit(wordsperMin,(correctWords.get_size()[0] + time.get_size()[0] + levelText.get_size()[0] + botGap*4,screenHeight-(bottomInfosHeight-time.get_size()[1]/2)))
+    screen.blit(wordsMissedTxt,(correctWords.get_size()[0] + time.get_size()[0] + levelText.get_size()[0] + wordsperMin.get_size()[0] + botGap*5,screenHeight-(bottomInfosHeight-time.get_size()[1]/2)))
+    if anouncer.fadeFlag and anouncer.fade == 0:
+        anouncer.display(screen,alpha)
+        alpha += anouncer.fadeSpeed
+        if alpha >= 255:
+            anouncer.fade = 255
+    elif anouncer.fadeFlag and anouncer.fade == 255:
+        anouncer.display(screen,alpha)
+        alpha -= anouncer.fadeSpeed
+        if alpha == 0:
+            anouncer.fadeFlag = False
+    
     pygame.display.update()
--- wordGenerator.py
+++ wordGenerator.py
@@ -13,9 +13,9 @@ class WordGenerator:
 class Word:
     def __init__(self,word,x,font,globalSpeed):
         self.wordX = x
-        self.wordY = randint(-100,0)
+        self.wordY = randint(-200,0)
         self.text = word
-        self.wordObj = font.render(self.text, True, (66, 255, 79))
+        self.wordObj = font.render(self.text, True, (179,179,179))
         self.WordWidth = font.size(self.text)[0]
         self.speed = len(self.text)
         
@@ -54,6 +54,39 @@ class Word:
 
     def focusColor(self,font,flag):
         if flag:
-            self.wordObj = font.render(self.text, True, (255, 255, 0))
+            self.wordObj = font.render(self.text, True, (0, 120, 215))
         elif not flag:
-            self.wordObj = font.render(self.text, True, (66, 255, 79))
+            self.wordObj = font.render(self.text, True, (179,179,179))
+
+class Anouncer:
+    def __init__(self,anouncerFont,swidth,sheight,botheight):
+        self.initialPrint = True
+        self.scrwidth = swidth
+        self.scrheight = sheight
+        self.botomheight = botheight
+        self.font = anouncerFont
+        self.anouncerColor = (179,179,179)
+        self.anouncerMessage = ""
+        self.anouncerPos = 0
+        self.anouncerTxt = self.font.render(self.anouncerMessage,True, self.anouncerColor)
+        self.fadeFlag = True
+        self.fade = 0
+        self.fadeSpeed = 2
+        self.anouncerTime = 0
+        self.anouncertimeTotal = 3
+
+    def changeText(self,text,fadeSpeed):
+        self.fadeFlag = True
+        self.fade = 0
+        self.fadeSpeed = fadeSpeed
+        self.anouncerMessage = text
+        self.anouncerTxt = self.font.render(self.anouncerMessage,True, self.anouncerColor)
+        self.anouncerPos = ((self.scrwidth-self.anouncerTxt.get_size()[0])/2,(self.scrheight-self.anouncerTxt.get_size()[1]-self.botomheight)/2)
+
+    def display(self,screen,alpha):
+        self.anouncerTxt.set_alpha(alpha)
+        if self.anouncerTxt:
+            if self.anouncerTime == self.anouncertimeTotal:
+                self.anouncerMessage = ""
+                self.anouncerTime = 0
+            screen.blit(self.anouncerTxt,self.anouncerPos)
